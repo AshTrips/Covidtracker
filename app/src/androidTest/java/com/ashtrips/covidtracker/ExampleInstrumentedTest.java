0b10959b78ac9b00f946a3d11e5495f5bb1c8224
AshTrips <55692410+AshTrips@users.noreply.github.com>
2020-07-25 23:09:13 +0530

    Add files via upload

---
+++ app/src/androidTest/java/com/ashtrips/covidtracker/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.ashtrips.covidtracker;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.shatrix.covi19monitor", appContext.getPackageName());
+    }
+}
